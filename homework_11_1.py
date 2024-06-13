188d54c4a1d1bc9aaddbcf9087ca307455db1d98
Anastasiya Solodukho <anastasiya.solodukho@gmail.com>
2024-06-13 22:34:30 +0200

    Added HW11

---
+++ homework_11_1.py
@@ -0,0 +1,100 @@
+"""Homework # 11"""
+# Task #1
+
+
+class Book:
+    """Create class Book"""
+
+    def __init__(self, name_book, author,
+                 isbn):
+        self.name_book = name_book
+        self.author = author
+        self.isbn = isbn
+        self.is_reserved = False
+        self.reserved_by = ""
+        self.is_taken = False
+        self.taken_by = ""
+
+
+class Client:
+    """Create class Client"""
+
+    def __init__(self, client_id, first_name, last_name):
+        self.client_id = client_id
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def reserve_book(self, book):
+        """Reserve book method"""
+        if not book.is_reserved:
+            book.is_reserved = True
+            book.reserved_by = self.client_id
+            print(f"\"{book.name_book}\" is reserved successfully")
+            return True
+        print(f"\"{book.name_book}\" is reserved by another client")
+        return True
+
+    def take_book(self, book):
+        """Take book method"""
+        if not book.is_taken and not book.is_reserved:
+            book.is_taken = True
+            book.taken_by = self.client_id
+            print(f"You can take book: \"{book.name_book}\"")
+            return True
+        if (not book.is_taken and book.is_reserved
+                and book.reserved_by == self.client_id):
+            book.is_taken = True
+            book.is_reserved = False
+            book.reserved_by = None
+            book.taken_by = self.client_id
+            print(f"Book was returned. "
+                  f"You can take reserved book: \"{book.name_book}\"")
+            return True
+        if book.is_taken and not book.is_reserved:
+            print(f"Book: \"{book.name_book}\" is read by another"
+                  f" client. You can reserve the book")
+            return True
+        if (book.is_taken and book.is_reserved
+                and book.reserved_by != self.client_id):
+            print(f"Book: \"{book.name_book}\" is read by another "
+                  f"client and is not available for reserving")
+            return True
+        if (book.is_taken and book.is_reserved
+                and book.reserved_by == self.client_id):
+            print(f"Book: \"{book.name_book}\" is read by another "
+                  f"client. Book \"{book.name_book}\" is reserved by you")
+            return True
+        return False
+
+    def return_book(self, book):
+        """Return book method"""
+        if book.is_taken and book.taken_by == self.client_id:
+            book.is_taken = False
+            book.taken_by = None
+            print(f"\"{book.name_book}\" is returned successfully")
+            return True
+        if book.is_taken and book.taken_by != self.client_id:
+            print(f"Error: \"{book.name_book}\" is not taken by you")
+            return True
+        print(f"Error: \"{book.name_book}\" is not taken")
+        return False
+
+
+book_1 = Book("Harry Potter 1", "J. K. Rowling", 1)
+book_2 = Book("Harry Potter 2", "J. K. Rowling", 2)
+book_3 = Book("The Lord of the Ring 1", "J. R. R. Tolkien", 3)
+book_4 = Book("The Lord of the Ring 2", "J. R. R. Tolkien", 4)
+
+client_1 = Client(1, "Ivan", "Ivanov")
+client_2 = Client(2, "Vasya", "Petrov")
+client_3 = Client(3, "Olia", "Pupkina")
+
+
+assert client_1.take_book(book_1), True
+assert client_2.take_book(book_1), True
+assert client_2.reserve_book(book_1), True
+assert client_3.take_book(book_1), True
+assert client_2.take_book(book_1), True
+assert client_3.return_book(book_1), True
+assert not client_3.return_book(book_3), False
+assert client_1.return_book(book_1), True
---
+++ homework_11_2.py
@@ -0,0 +1,33 @@
+"""Homework # 11"""
+# Task #2
+
+from decimal import Decimal
+
+
+class Deposit:
+    """Create class Deposit"""
+
+    def __init__(self, deposit_amount, deposit_time, percent):
+        self.deposit_amount = Decimal(str(deposit_amount))
+        self.deposit_time = deposit_time
+        self.percent = Decimal(str(percent))
+
+
+class Bank:
+    """Create class Bank"""
+
+    def method_deposit(self, deposit):
+        """Calculate the amount that will be on the user's account"""
+        count_of_months = 0
+        while count_of_months < deposit.deposit_time:
+            deposit.deposit_amount += (deposit.deposit_amount
+                                       * deposit.percent / 100)
+            count_of_months += 1
+        print(f"Total amount: {round(deposit.deposit_amount, 2)} BYN")
+        return True
+
+
+deposit_1 = Deposit(1000, 12, 10)
+bank_1 = Bank()
+
+assert bank_1.method_deposit(deposit_1), True
