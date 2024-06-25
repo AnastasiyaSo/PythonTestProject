515c17bd6f03cdb70eac727b8a85218fd546d236
Anastasiya Solodukho <anastasiya.solodukho@gmail.com>
2024-06-25 21:14:04 +0200

    NW 14_1 was fixed

--- homework/homework_14/homework_14_1.py
+++ homework/homework_14/homework_14_1.py
@@ -2,33 +2,42 @@
 # Task # 1
 
 try:
+    students_data = [
+        "Serena Williams", "1A", "[5,4,4,3]",
+        "\nLionel Messi", "2B", "[4,3,5,2]",
+        "\nKobe Bryant", "1A", "[3,5,4,1]"
+    ]
+
     with open("students.txt", "w+", encoding="utf-8") as students_file:
-        content = students_file.write("student1 group1 5\n"
-                                      "student2 group1 5\n"
-                                      "student3 group1 5\n"
-                                      "student4 group2 5\n"
-                                      "student5 group2 4\n"
-                                      "student6 group2 3\n")
+        students_file.write(" ".join(students_data))
 
     with open("students.txt", "r", encoding="utf-8") as students_file:
         content1 = students_file.readlines()
-        group1, marks1 = 0, 0
-        group2, marks2 = 0, 0
+        group1, marks1 = 0, 0.0
+        group2, marks2 = 0, 0.0
         for line in content1:
-            student, group, mark = line.split()
-            if group == "group1":
+            first_name, last_name, group, marks = line.split()
+            marks_ = marks.strip("[ ]")
+            marks_2 = marks_.split(",")
+            marks_list = list(map(float, marks_2))
+            number_of_marks = len(marks_list)
+            if group == "1A":
                 group1 += 1
-                marks1 += int(mark)
-            if group == "group2":
+                marks1 += sum(marks_list) / number_of_marks
+            if group == "2B":
                 group2 += 1
-                marks2 += int(mark)
+                marks2 += sum(marks_list) / number_of_marks
         total_students = group1 + group2
-        marks1 = int(marks1 / group1)
-        marks2 = int(marks2 / group2)
-        print(total_students, marks1, marks2)
+        total_marks1 = float(marks1 / group1)
+        total_marks2 = float(marks2 / group2)
+        print(total_students, total_marks1, total_marks2)
 
     with open("students.txt", "a", encoding="utf-8") as students_file:
-        content2 = students_file.write(f"{total_students}, {marks1}, {marks2}")
+        content2 = students_file.write(f"\nTotal students: {total_students}"
+                                       f", Average rating of"
+                                       f" \"1A\": {total_marks1}, "
+                                       f"Average rating of"
+                                       f" \"2B\": {total_marks2}")
 
 except FileNotFoundError:
     print("FileNotFoundError")
