6233fbf11432708171d2d26f7e45c46a11238b3a
Anastasiya Solodukho <anastasiya.solodukho@gmail.com>
2024-07-26 12:07:47 +0200

    Homework 23 was added

---
+++ homework/homework_23.py
@@ -0,0 +1,43 @@
+"""Contactlist"""
+
+# On the Login page:
+# Enter email in field: #email
+# Enter password in field: #password
+# Click on the button: #submit
+
+# 1. Create a new contact:
+# Page https://thinking-tester-contact-list.herokuapp.com/contactList should be opened
+# Click on the button: #add-contact
+# Enter First name (required): #firstName
+# Enter Last name (required): #lastName
+# Enter Date of Birth (optional): #birthdate
+# Enter phone (optional): #phone
+# Enter street address 1 (optional): #street1
+# Enter street address 2 (optional): #street2
+# Enter city (optional): #city
+# Enter state or province (optional): #stateProvince
+# Enter postal code (optional): #postalCode
+# Enter country (optional): #country
+# Click on the button: #submit
+
+# 2. Edit the contact:
+# Page https://thinking-tester-contact-list.herokuapp.com/contactList should be opened
+# Click on the row table: //tr[@class="contactTableBodyRow"][1]
+# Click on the button: #edit-contact
+# Change First name: #firstName
+# Change Last name: #lastName
+# Change Date of Birth: #birthdate
+# Change phone: #phone
+# Change street address 1: #street1
+# Change street address 2: #street2
+# Change city: #city
+# Change state or province: #stateProvince
+# Change postal code: #postalCode
+# Change country: #country
+# Click on the button: #submit
+
+# 3. Delete the contact:
+# Page https://thinking-tester-contact-list.herokuapp.com/contactList should be opened
+# Click on the row table: //tr[@class="contactTableBodyRow"][1]
+# Click on the button: #delete
+# Press "Enter" on the keyboard
